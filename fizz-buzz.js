268e1e7c3f8ac4ccf4668815fe8ebd466eb9f0f3
Christopher Palileo <cpalileo@gmail.com>
2022-09-02 11:53:55 -0700

    FizzBuzz

---
+++ fizz-buzz.js
@@ -0,0 +1,23 @@
+// Write code to loop through the array of numbers
+// At each iteration, if a number is evenly divisible by 3 print "Fizz"
+// If a number is evenly divisible by 5 print "Buzz"
+// If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
+// If a number is not divisible by 3 or 5, print the number
+
+var fizzBuzz = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let result = arr[i];
+    // let threeModal = result % 3;
+    // let fiveModal = result % 5;
+
+    if (result % 3 === 0) {
+      console.log("Fizz");
+    } else if (result %5  === 0) {
+      console.log("Buzz");
+    } else if (result % 3 === 0 && result % 5 === 0) {
+      console.log("Fizz Buzz");
+    } else {
+      console.log(result);
+    }
+  }
+};
