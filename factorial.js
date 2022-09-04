00df309cf9284efc8b78c8d5d3c531a1678e7d6f
Christopher Palileo <cpalileo@gmail.com>
2022-09-04 16:24:35 -0700

    factoral

---
+++ factorial.js
@@ -0,0 +1,15 @@
+// Write code to create a function that returns the factorial of `num`
+
+var factorial = function (num) {
+  // Create empty array
+  let list = [];
+
+  // Push elements from 0 to num into the array
+  for (var i = 0; i <= num; i++) {
+    list.push(i);
+
+    // Add all elements in the array
+
+    console.log(list.length);
+  }
+};
